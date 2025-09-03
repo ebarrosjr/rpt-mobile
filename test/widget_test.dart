c800da1de340a34968624e62f30388e108b6cf82
Everton C B jr <ebarrosjr@gmail.com>
2025-09-03 07:24:02 -0300

    Login Único e Push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:rptmobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const RptApp());
+    await tester.pumpWidget(RptApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
